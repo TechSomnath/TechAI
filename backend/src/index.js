31cba390321676a1ea514a0ac93cf623d02d6ae0
Somnath Ghosh <sg1941627@gmail.com>
2026-01-17 21:21:44 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://techai-frontend.onrender.com",
+    origin:process.env.FRONTEND_URL,
     credentials: true,
   })
 );
