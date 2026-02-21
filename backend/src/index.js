182c6eb8d2b8c882b3321ae0fd75ccada0aa0d4c
Somnath Ghosh <sg1941627@gmail.com>
2026-02-22 00:23:57 +0530

    Update CORS origin to techai-1.onrender.com

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:process.env.FRONTEND_URL,
+    origin:"https://techai-1.onrender.com",
     credentials: true,
   })
 );
